0dd6dd8d93cd50b38b852bce38a1171d1e4a0c77
Yangzhou Wang <nested4528@gmail.com>
2019-03-22 17:23:50 +0800

    Initial Commit

---
+++ infoEnc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  infoEnc
+//
+//  Created by 王大头 on 2019/3/22.
+//  Copyright © 2019 unclesnake. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
