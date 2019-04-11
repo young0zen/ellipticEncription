eae0214e097606bdacdceef270c4df7060a803db
Yangzhou Wang <nested4528@gmail.com>
2019-04-11 19:16:06 +0800

    new folder

---
+++ shamir/shamir/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  shamir
+//
+//  Created by ScottWang on 2019/4/11.
+//  Copyright © 2019 unclesnake. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
