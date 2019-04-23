9b26e061da2845c65f2bac59e3db5b6bd3fc3fc5
Yangzhou Wang <nested4528@gmail.com>
2019-04-23 21:38:45 +0800

    a server that can handle dual-direction messages

--- socks/server.py
+++ socks/server.py
@@ -5,14 +5,13 @@ import sys
 import getopt
 from _thread import *
 import selectors
-import time
 
 sel = selectors.DefaultSelector()
 
-host = "localhost"
+#host = "localhost"
 port = 23456
-isServer = False
 
+# A map from connection id to two-tuple (client_socket, to_remote_socket)
 connection_map_dic = {}
 
 def setup_socket(family, type):
@@ -23,7 +22,6 @@ def setup_socket(family, type):
         sys.exit(1)
     return sock
 
-
 def connect_to_server(host, port):
     sock = setup_socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -31,56 +29,94 @@ def connect_to_server(host, port):
     except socket.gaierror:
         print ("Host name could not be resolved. Aborting...")
         sock.close()
-        sys.exit(1)
+        return -1
 
-    sock.connect((remote_addr, port))
-    sel.register(sock, selectors.EVENT_READ, read)
+    try:
+        sock.connect((remote_addr, port))
+    except socket.error:
+        print ("Connection failed.")
+        sock.close()
+        return -1;
+
+    return sock
 
 def accept(sock, mask):
     conn, addr = sock.accept()  # Should be ready
     print('accepted', conn, 'from', addr)
     conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, read)
+    sel.register(conn, selectors.EVENT_READ, (read, get_available_connection_id()))
 
-def read(conn, mask):
+def read(conn, id):
+    # print (conn, "id:", id)
+
     data = conn.recv(65536)  # Should be ready
-    print (type(data))
-    if data:
-        conn.send(data)  # Hope it won't block
+    if not data: # eof or error
+        socket_pair = connection_map_dic.get(id, None)
+        if (socket_pair == None): # not in the dictionary
+            print ("Unregistering one")
+            sel.unregister(conn)
+            conn.close()
+        else:
+            print ("Unregistering both")
+            sel.unregister(socket_pair[0])
+            sel.unregister(socket_pair[1])
+            socket_pair[0].close()
+            socket_pair[1].close()
+            del connection_map_dic[id]
     else:
-        # print('closing', conn)
-        sel.unregister(conn)
-        conn.close()
+        socket_pair = connection_map_dic.get(id, None)
+        if (socket_pair == None): # not in the dictionary
+            msg = data.decode("utf-8")
+            msg = msg.strip()
+            host_port = msg.split(':')
+            print ("Connecting to " + host_port[0] + ", port " + host_port[1])
+            to_server_sock = connect_to_server(host_port[0], int(host_port[1]))
+            if (to_server_sock == -1):
+                print ("Cannot connect to server.")
+            else:
+                connection_map_dic[id] = (conn, to_server_sock)
+                sel.register(to_server_sock, selectors.EVENT_READ, (read, id))
+        # send it to the other side
+        elif (conn == socket_pair[0]):
+            socket_pair[1].send(data)
+        elif (conn == socket_pair[1]):
+            socket_pair[0].send(data)
+
+
+connection_id = 0
+def get_available_connection_id():
+    global connection_id
+    connection_id = connection_id + 1
+    return connection_id
 
 # main
-opts, args = getopt.getopt(sys.argv[1:], "h:p:s")
+opts, args = getopt.getopt(sys.argv[1:], "h:p:")
 for op, value in opts:
     if op == "-h":
-        host = value
+        pass
+        #host = value
     elif op == "-p":
         port = int(value)
-    elif op == "-s":
-        isServer = True
 
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except (socket.error, message):
+except (socket.error, message): #TODO: check python3 grammer
     print (message)
     sys.exit(1)
 
 try:
     sock.bind(('', port))
-except (socket.error, message):
-    print ("Bind failed. Error " + str(message[0]) + ": " + message[1])
+except (socket.error):
+    print ("Bind failed.")
     sock.close()
     sys.exit(1)
 
 sock.listen(100)
 sock.setblocking(False)
-sel.register(sock, selectors.EVENT_READ, accept)
+sel.register(sock, selectors.EVENT_READ, (accept, 0))
 
 while True:
     events = sel.select()
     for key, mask in events:
-        callback = key.data
-        callback(key.fileobj, mask)
+        callback = key.data[0]
+        callback(key.fileobj, key.data[1])
